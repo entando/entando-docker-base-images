3ca19a882b5159c83fad8b3c687dd47e8c6a2337
Ampie Barnard <a.barnard@entando.com>
2019-07-04 21:08:48 +0200

    Removed invalid volume mapping

--- sample-maven-project/run-smoke-tests.sh
+++ sample-maven-project/run-smoke-tests.sh
@@ -16,18 +16,16 @@ mvn clean package -P${JEE_SERVER} -P${DBMS} docker:start -Doracle.repo.user=${OR
 #mvn -P${JEE_SERVER} -P${DBMS} docker:start || { echo "Maven build failed"; exit 1; }
 docker run --rm --network=${APPLICATION_NAME}-network \
     -e ENTANDO_APPBUILDER_URL=http://appbuilder:5000  \
-    -v /home/lulu/Code.bup/entando/entando-QE/Selenium_APPBUILDER:/entando-smoke-tests \
     entando/entando-smoke-tests:5.2.0-SNAPSHOT mvn verify -Dtest=org.entando.selenium.smoketests.STAddTestUserTest \
     || { echo "AddTestUser smoke test failed"; exit 2; }
 #mvn -P${JEE_SERVER} -P${DBMS} docker:stop || { echo "Stopping containers from Maven failed"; exit 3; }
 #mvn -P${JEE_SERVER} -P${DBMS} docker:start  -Doracle.repo.user=${ORACLE_REPO_USER} && \
 #        -Doracle.repo.password=${ORACLE_REPO_PASSWORD} || { echo "Restarting containers from Maven failed"; exit 4; }
-#docker run --rm --network=${APPLICATION_NAME}-network \
-#    -e ENTANDO_ENGINE_URL=http://${APPLICATION_NAME}-engine:8080/${APPLICATION_NAME} \
-#    -e ENTANDO_APPBUILDER_URL=http://appbuilder:5000  \
-#    -v /home/lulu/Code.bup/entando/entando-QE/Selenium_APPBUILDER:/entando-smoke-tests \
-#    entando/entando-smoke-tests:5.2.0-SNAPSHOT mvn verify -Dtest=org.entando.selenium.smoketests.STLoginWithTestUserTest \
-#    || { echo "LoginWithTestUser smoke test failed"; exit 5; }
+docker run --rm --network=${APPLICATION_NAME}-network \
+    -e ENTANDO_ENGINE_URL=http://${APPLICATION_NAME}-engine:8080/${APPLICATION_NAME} \
+    -e ENTANDO_APPBUILDER_URL=http://appbuilder:5000  \
+    entando/entando-smoke-tests:5.2.0-SNAPSHOT mvn verify -Dtest=org.entando.selenium.smoketests.STLoginWithTestUserTest \
+    || { echo "LoginWithTestUser smoke test failed"; exit 5; }
 
 #post-test cleanup
 mvn -P${JEE_SERVER} -P${DBMS} docker:stop 2>/dev/null
