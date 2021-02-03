6dc51c67d24ce00e5c0227516263d62f09e82797
Ampie Barnard <ampie.private@gmail.com>
2021-02-03 11:43:50 +0200

    Rebased EAP7.3 standalone-opnshift.xml to latest

--- entando-eap73-clustered-base/build.sh
+++ entando-eap73-clustered-base/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export VERSION=${1:-5.0.3-SNAPSHOT}
-export ENTANDO_IMAGE="entando-eap72-clustered-base"
+export VERSION=${1:-6.3.1}
+export ENTANDO_IMAGE="entando-eap73-clustered-base"
 source ../common/build-common.sh
